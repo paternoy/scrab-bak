ffa52aa76adf85a0a58c1b11931c3a38e9b725aa
Jaume Paternoy <paternoy@gmail.com>
2013-05-29 01:45:24 +0200

    Añadido método HTTP POST a API Rest

--- scrab/scrab-rest/src/main/java/com/coopnex/scrab/rest/controller/CrudController.java
+++ scrab/scrab-rest/src/main/java/com/coopnex/scrab/rest/controller/CrudController.java
@@ -3,11 +3,13 @@ package com.coopnex.scrab.rest.controller;
 import java.io.Serializable;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.coopnex.scrab.service.CrudService;
 
@@ -31,7 +33,14 @@ public abstract class CrudController<T,PK extends Serializable>{
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public @ResponseBody
 	T create(@PathVariable long id,@RequestBody T element) {
+		//FIXME: use id
 		return getService().create(element);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED )
+	public void add(@RequestBody T element) {
+		getService().create(element);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
